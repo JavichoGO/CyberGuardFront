6cfceaa82403d53a45c3a747d6560648ac8772e0
airomerick98 <airomerick9817@gmail.com>
2024-09-26 11:15:27 -0500

    subir cambio rama

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,67 +13,69 @@ import pageAnswerQuestions from '@/pages/pageAnswerQuestions.vue';
 import resetPassword from '@/pages/restartPassword.vue';
 
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/home',
+    name: 'home-user',
+    children: [
+      {
+        path: '/gestor-user',
+        name: 'gestors-user',
+        component: gestorsUser
+      },
+      {
+        path: '/register-user/:id?',
+        name: 'register-user',
+        component: registerUser
+      },
+      {
+        path: '/register-question/:id?',
+        name: 'register-question',
+        component: registerQuestion
+      },
+      {
+        path: '/metrics-management',
+        name: 'metrics-management',
+        component: metricsManagement
+      },
+      {
+        path: '/gestor-question',
+        name: 'gestors-question',
+        component: gestorsQuestion
+      },
+      {
+        path: '/view-password',
+        name: 'view-password',
+        component: viewPassword
+      },
+      {
+        path: '/respuest',
+        name: 'respuest',
+        component: viewForm
+      },
+      {
+        path: '/answer-question',
+        name: 'answer-question',
+        component: pageAnswerQuestions,
+      },
+      {
+        path: '/reset-password',
+        name: 'reset-password',
+        component: resetPassword,
+      },
+    ],
+    component: MasterPage
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/home',
-      name: 'home-user',
-      children: [
-        {
-          path: '/gestor-user',
-          name: 'gestors-user',
-          component: gestorsUser
-        },
-        {
-          path: '/register-user/:id?',
-          name: 'register-user',
-          component: registerUser
-        },
-        {
-          path: '/register-question/:id?',
-          name: 'register-question',
-          component: registerQuestion
-        },
-        {
-          path: '/metrics-management',
-          name: 'metrics-management',
-          component: metricsManagement
-        },
-        {
-          path: '/gestor-question',
-          name: 'gestors-question',
-          component: gestorsQuestion
-        },
-        {
-          path: '/view-password',
-          name: 'view-password',
-          component: viewPassword
-        },
-        {
-          path: '/respuest',
-          name: 'respuest',
-          component: viewForm
-        },
-        {
-          path: '/answer-question',
-          name: 'answer-question',
-          component: pageAnswerQuestions,
-        },
-        {
-          path: '/reset-password',
-          name: 'reset-password',
-          component: resetPassword,
-        },
-      ],
-      component: MasterPage
-    },
-  ]
+  routes,
 })
 
 export default router
