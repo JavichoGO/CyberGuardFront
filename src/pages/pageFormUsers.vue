c4940fc09a08065dd0bbfcecf01979fc0da06668
Javier Garcia <javier.garcia@aaa.com>
2024-10-27 09:21:15 -0500

    botón Reiniciar Test

--- src/pages/pageFormUsers.vue
+++ src/pages/pageFormUsers.vue
@@ -3,7 +3,6 @@ import { onMounted, ref } from 'vue';
 import { useForm } from '../composables/useForm';
 import { useRouter, useRoute } from 'vue-router';
 import { useFormStore } from '../stores/useFormStore';
-import  { storeToRefs } from 'pinia';
 import Modal from '../utils/appModal.vue';
 
 const { fetchQuestions, getShowFinished, fetchUpdate } = useForm();
@@ -12,82 +11,88 @@ const router = useRouter();
 const route = useRoute();
 
 const showValidCompleted = ref(false);
-
-const validationPage = ref(route.query.isSolution)
-
-
+const validationPage = ref(route.query.isSolution);
 const showModal = ref(false);
+const buttonLabel = ref('Iniciar Test'); // Inicializar con "Iniciar Test"
+const isLoading = ref(true); // Bandera para determinar si se está cargando
+
+
+// Función para actualizar el texto del botón según el estado del test
+const updateButtonLabel = async () => {
+    const isFinished = getShowFinished.value;
+    buttonLabel.value = isFinished ? 'Reiniciar Test' : 'Iniciar Test';
+    isLoading.value = false; // Termina la carga después de obtener el valor
+
+};
 
 const postForm = async () => {
-    await fetchUpdate();
-    await fetchQuestions();
-    router.push({ name: 'answer-question' });
-}
+  await fetchUpdate();
+  await fetchQuestions();
+  router.push({ name: 'answer-question' });
+};
 
 const openDialog = () => {
-    if (getShowFinished.value) { 
-        showValidCompleted.value = true;
-    } else {
-        showModal.value = true;
-    }
-}
+  if (getShowFinished.value) {
+    showValidCompleted.value = true;
+  } else {
+    showModal.value = true;
+  }
+};
 
 const resetQuestionFunction = () => {
-    postForm();
-}
+  postForm();
+};
 
 onMounted(async () => {
   await fetchQuestions();
-})
+  await updateButtonLabel(); // Actualizar el texto del botón al cargar la página
+});
 </script>
 
-
 <template>
-    <div class="mt-2">
-        <h1 class="text-4xl font-bold text-gray-900 text-center mb-10 mt-10">Cuestionario PWC</h1>
-        <div v-if="validationPage">
-            <h2 class="text-2xl text-center">
-                MUCHAS GRACIAS POR REALIZAR LA ENCUESTA !
-            </h2>
-            <div class="flex justify-center mt-5">
-                <button
-                @click="router.push({ name: 'metrics-management' });"
-                class=" bg-blue-500 text-white py-2 px-4 rounded-md shadow-sm  focus:outline-none"
-            >
-              VER MÉTRICAS
-            </button>
-            </div> 
-        </div>
-        <div v-else>
-            <h2 class="text-2xl text-center text-justify mx-auto" style="max-width: 800px;">
+  <div class="mt-2">
+    <h1 class="text-4xl font-bold text-gray-900 text-center mb-10 mt-10">Cuestionario PWC</h1>
+    <div v-if="validationPage">
+      <h2 class="text-2xl text-center">
+        MUCHAS GRACIAS POR REALIZAR LA ENCUESTA !
+      </h2>
+      <div class="flex justify-center mt-5">
+        <button
+          @click="router.push({ name: 'metrics-management' });"
+          class="bg-blue-500 text-white py-2 px-4 rounded-md shadow-sm focus:outline-none"
+        >
+          VER MÉTRICAS
+        </button>
+      </div> 
+    </div>
+    <div v-else>
+      <h2 class="text-2xl text-center text-justify mx-auto" style="max-width: 800px;">
         Este formulario está diseñado para determinar el grado de madurez de las Cajas Municipales de Ahorro y Crédito (CMAC) en el Perú a través de su punto de vista como integrante de la empresa auditora PWC.<br><br>
         Por favor, tenga en cuenta que las preguntas están dirigidas únicamente a recoger su opinión personal y aseguramos la completa confidencialidad de sus respuestas. Los datos recopilados se utilizarán exclusivamente con fines académicos.
-    </h2>
-             <div class="flex justify-end mt-5">
-                
-
-
-                <button
-                @click="openDialog"
-                class="bg-blue-500 text-white py-2 px-4 rounded-md shadow-sm  focus:outline-none"
-            >
-             Iniciar test
-            </button>
-            </div> 
-        </div>
-        <Modal
-            :isOpen="showModal"
-            title=""
-            message="Deseas iniciar el test ?"
-            @update:isOpen="showModal = $event"
-            @confirm="postForm"
-          />
-          <Modal
-          :isOpen="showValidCompleted"
-          title="Desea realizar nuevamente el cuestionario ?"
-          message="Se eliminará la encuesta realizada previamente"
-          @update:isOpen="showValidCompleted = $event"
-          @confirm="resetQuestionFunction"
-        />
+      </h2>
+      <div class="flex justify-end mt-5">
+        <button
+        v-if="!isLoading" 
+          @click="openDialog"
+          class="bg-blue-500 text-white py-2 px-4 rounded-md shadow-sm focus:outline-none"
+        >
+          {{ buttonLabel }}
+        </button>
+      </div> 
     </div>
+    <Modal
+      :isOpen="showModal"
+      title=""
+      message="Deseas iniciar el test ?"
+      @update:isOpen="showModal = $event"
+      @confirm="postForm"
+    />
+    <Modal
+      :isOpen="showValidCompleted"
+      title="Desea realizar nuevamente el cuestionario ?"
+      message="Se eliminará la encuesta realizada previamente"
+      @update:isOpen="showValidCompleted = $event"
+      @confirm="resetQuestionFunction"
+    />
+  </div>
 </template>
